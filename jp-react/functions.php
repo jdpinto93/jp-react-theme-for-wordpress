a1c6067f3ec89b9480c616359cc3666540b0b1e8
Jose Pinto <69821893+jdpinto93@users.noreply.github.com>
2023-02-13 11:26:37 +0000

    Ron Start

--- jp-react/functions.php
+++ jp-react/functions.php
@@ -182,3 +182,15 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+// Enqueue Theme JS w React Dependency
+add_action( 'wp_enqueue_scripts', 'my_enqueue_theme_js' );
+function my_enqueue_theme_js() {
+  wp_enqueue_script(
+    'my-theme-frontend',
+    get_stylesheet_directory_uri() . '/build/index.js',
+    ['wp-element'],
+    time(), // Change this to null for production
+    true
+  );
+}
